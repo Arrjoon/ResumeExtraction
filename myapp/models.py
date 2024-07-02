e85de894c6510d95f6aace723d907b8e1b4ea606
Arrjoon <nepaliarjun049@gmail.com>
2024-07-02 09:34:37 +0545

    adding resume_descritpion model

--- myapp/models.py
+++ myapp/models.py
@@ -3,21 +3,38 @@ from django.db import models
 # Create your models here.
 # models.py
 
-from django.db import models
-
-class JobDescription(models.Model):
+class Job_Description(models.Model):
     job_role = models.CharField(max_length=50)
     company_description = models.TextField()
     role_description = models.TextField()
-    qualification = models.TextField()
-    Experience = models.IntegerField()
-
+    qualification = models.TextField(null=True)
+    degree = models.CharField(max_length=100,null=True)
+    experience = models.IntegerField()
+    skills = models.TextField(help_text="Comma-separated list of skills")
     def __str__(self):
         return self.job_role
+    def get_skills_list(self):
+        return self.skills.split(',')
 
-class Skill(models.Model):
-    job_description = models.ForeignKey(JobDescription, related_name='skills', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+
+
+
+
+class Resume_Description(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    designation = models.CharField(max_length=100,null=True)
+    location = models.CharField(max_length=100,null=True)
+    email_address = models.EmailField(null=True)
+    company_working_at = models.CharField(max_length=100,null=True)
+    degree = models.CharField(max_length=100,null=True)
+    college_name = models.CharField(max_length=100,null=True)
+    graduation_year = models.CharField(max_length=20,null=True)
+    experience = models.CharField(max_length=20,null=True)
+    university = models.CharField(max_length=25,null=True)
+    skills = models.TextField(help_text="Comma-separated list of skills")
 
     def __str__(self):
         return self.name
+
+    def get_skills_list(self):
+        return self.skills.split(',')
